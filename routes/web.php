c0545be96794b790d3da3196d237826d5b5393bf
rurururiri <165371710+resyaaaaa@users.noreply.github.com>
2025-12-18 18:37:12 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-
-// CRUD routes for donors and donations
+});
 Route::resource('donors', DonorController::class);
 Route::resource('donations', DonationController::class);
